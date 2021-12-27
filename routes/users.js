462cf8cb976716cc6bfbd430d698f2a1746df999
Julien Sebag <julien.sebag@me.com>
2021-12-27 15:17:22 +0100

    Added build

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const User = require("../models/User.model")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 
 const fileUploader = require("../config/cloudinary.config")
 
