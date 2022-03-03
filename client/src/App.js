1bdc1406daffd31f75f52fda509b0c4183684c29
Julien <julien.sebag@me.com>
2022-03-03 01:18:01 +0100

    Update components library

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,55 @@
 // Packages
-import React from "react"
-import { GlobalStyles } from "components-react-julseb"
+import React, { useState } from "react"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 // Components
-import Switch from "./routes/Switch"
+import routes from "./routes/routes"
+import redirects from "./routes/redirects"
+import ProtectedRoutes from "./routes/ProtectedRoutes"
+import AnonRoutes from "./routes/AnonRoutes"
 
 const App = () => {
+    const [edited, setEdited] = useState(false)
+
     return (
-        <>
-            <GlobalStyles />
-            <Switch />
-        </>
+        <Routes>
+            {routes.map((route, i) => (
+                <Route
+                    path={route.path}
+                    element={
+                        route.protected ? (
+                            <ProtectedRoutes>
+                                <route.element
+                                    edited={route.edit && edited}
+                                    setEdited={route.edit && setEdited}
+                                />
+                            </ProtectedRoutes>
+                        ) : route.anon ? (
+                            <AnonRoutes>
+                                <route.element
+                                    edited={route.edit && edited}
+                                    setEdited={route.edit && setEdited}
+                                />
+                            </AnonRoutes>
+                        ) : (
+                            <route.element
+                                edited={route.edit && edited}
+                                setEdited={route.edit && setEdited}
+                            />
+                        )
+                    }
+                    key={i}
+                />
+            ))}
+
+            {redirects.map((route, i) => (
+                <Route
+                    path={route.path}
+                    element={<Navigate to={route.to} />}
+                    key={i}
+                />
+            ))}
+        </Routes>
     )
 }
 
